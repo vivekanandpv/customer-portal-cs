7869feab38fa5bf14322c4098b5b4e17dbd82957
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-08 18:23:52 +0530

    cors resolution

--- src/main/java/com/example/customerportal/apis/AccountApi.java
+++ src/main/java/com/example/customerportal/apis/AccountApi.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/accounts")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class AccountApi {
     private final AccountService accountService;
 
--- src/main/java/com/example/customerportal/apis/CustomerApi.java
+++ src/main/java/com/example/customerportal/apis/CustomerApi.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/customers")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class CustomerApi {
     private final CustomerService customerService;
 
